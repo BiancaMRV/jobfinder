988da9c0cae370e9320584eb35b8ebed08680c7a
Bianca Vilaverde <biancamargarida2014@gmail.com>
2024-10-25 23:52:50 +0100

    application model

--- server/routes/applicationRoutes.ts
+++ server/routes/applicationRoutes.ts
@@ -8,83 +8,118 @@ import {
   updateAplicationResume,
   updateApplicationStatus,
 } from "../controllers/applicationcontrollers";
+import {
+  createUserValidation,
+  deleteUserValidation,
+  getUserValidation,
+  updateUserValidation,
+  validateRequest,
+} from "../middleware/validationMiddleware";
 
 const routers = express.Router();
 
-routers.get("/application", async (req, res) => {
-  try {
-    const application = getAllApplication();
-    res.send(application);
-  } catch (error) {
-    res.status(500).send("Error retrieving applications");
+routers.get(
+  "/application",
+  validateRequest(getUserValidation),
+  async (req, res) => {
+    try {
+      const application = getAllApplication();
+      res.send(application);
+    } catch (error) {
+      res.status(500).send("Error retrieving applications");
+    }
   }
-});
+);
 
-routers.get("/application/:applicationId", async (req, res) => {
-  try {
-    const { applicationId } = req.params; // Extract applicationId from the request body
-    const application = await getAllApplicationbyId(applicationId); // Pass applicationId as an argument
-    res.send(application);
-  } catch (error) {
-    res.status(500).send("Error retrieving application by ID");
+routers.get(
+  "/application/:applicationId",
+  validateRequest(getUserValidation),
+  async (req, res) => {
+    try {
+      const { applicationId } = req.params; // Extract applicationId from the request body
+      const application = await getAllApplicationbyId(applicationId); // Pass applicationId as an argument
+      res.send(application);
+    } catch (error) {
+      res.status(500).send("Error retrieving application by ID");
+    }
   }
-});
+);
 
-routers.delete("/application", async (req, res) => {
-  try {
-    const { applicationId } = req.body;
-    const application = await deleteApplication(applicationId);
-    res.send(application);
-  } catch (error) {
-    res.status(500).send("Error deleting application");
+routers.delete(
+  "/application",
+  validateRequest(deleteUserValidation),
+  async (req, res) => {
+    try {
+      const { applicationId } = req.body;
+      const application = await deleteApplication(applicationId);
+      res.send(application);
+    } catch (error) {
+      res.status(500).send("Error deleting application");
+    }
   }
-});
+);
 
-routers.post("/application", async (req, res) => {
-  try {
-    const { name, cover_letter, resume, job_offer_id, company_id } = req.body;
-    const application = await createNewApplication(
-      name,
-      cover_letter,
-      resume,
-      job_offer_id,
-      company_id
-    );
-    res.send(application);
-  } catch (error) {
-    res.status(500).send("Error creating application");
+routers.post(
+  "/application",
+  validateRequest(createUserValidation),
+  async (req, res) => {
+    try {
+      const { name, cover_letter, resume, job_offer_id, company_id } = req.body;
+      const application = await createNewApplication(
+        name,
+        cover_letter,
+        resume,
+        job_offer_id,
+        company_id
+      );
+      res.send(application);
+    } catch (error) {
+      res.status(500).send("Error creating application");
+    }
   }
-});
+);
 
-routers.patch("/application/:applicationId/cover_letter", async (req, res) => {
-  try {
-    const { cover_letter, applicationId } = req.body;
-    const application = await updateAplicationResume(
-      cover_letter,
-      applicationId
-    );
-    res.send(application);
-  } catch (error) {
-    res.status(500).send("Error updating application");
+routers.patch(
+  "/application/:applicationId/cover_letter",
+  validateRequest(updateUserValidation),
+  async (req, res) => {
+    try {
+      const { cover_letter, applicationId } = req.body;
+      const application = await updateAplicationResume(
+        cover_letter,
+        applicationId
+      );
+      res.send(application);
+    } catch (error) {
+      res.status(500).send("Error updating application");
+    }
   }
-});
+);
 
-routers.patch("/application/:applicationId/status", async (req, res) => {
-  try {
-    const { status, applicationId } = req.body;
-    const application = await updateApplicationStatus(status, applicationId);
-    res.send(application);
-  } catch (error) {
-    res.status(500).send("Error updating application");
+routers.patch(
+  "/application/:applicationId/status",
+  validateRequest(updateUserValidation),
+  async (req, res) => {
+    try {
+      const { status, applicationId } = req.body;
+      const application = await updateApplicationStatus(status, applicationId);
+      res.send(application);
+    } catch (error) {
+      res.status(500).send("Error updating application");
+    }
   }
-});
+);
 
-routers.patch("/application/:applicationId/resume", async (req, res) => {
-  try {
-    const { resume, applicationId } = req.body;
-    const application = await updateAplicationResume(resume, applicationId);
-    res.send(application);
-  } catch (error) {
-    res.status(500).send("Error updating application");
+routers.patch(
+  "/application/:applicationId/resume",
+  validateRequest(updateUserValidation),
+  async (req, res) => {
+    try {
+      const { resume, applicationId } = req.body;
+      const application = await updateAplicationResume(resume, applicationId);
+      res.send(application);
+    } catch (error) {
+      res.status(500).send("Error updating application");
+    }
   }
-});
+);
