847e3ed59fb673e54ad31d38c078c70bb61a89b3
Bianca Vilaverde <biancamargarida2014@gmail.com>
2024-10-31 20:33:04 +0000

    changes in signUp

--- client/src/pages/Auth/SignUp.tsx
+++ client/src/pages/Auth/SignUp.tsx
@@ -4,30 +4,40 @@ export function SignUp() {
   return (
     <>
       <div className={styles.container}>
-        {/* Secção da Imagem */}
+        <div className={styles.imageSection}>
+          <div className={styles.imageText}>
+            <h2> Create your future </h2>
+          </div>
 
-        <h1>Registar Conta</h1>
-        <form action="">
-          <label htmlFor="firstName">Nome</label>
-          <input
-            type="text"
-            id="firstName"
-            autoComplete="given-name"
-            className={styles.firstName}
-          />
-          <label htmlFor="lastName">Apelido</label>
-          <input
-            type="text"
-            id="lastName"
-            autoComplete="family-name"
-            className={styles.lastName}
-          />
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" className={styles.email} />
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" className={styles.password} />
-          <button>Registar conta</button>
-        </form>
+          <h1>Registar Conta</h1>
+          <form action="">
+            <div className={styles.formSection}>
+              <label htmlFor="firstName">Nome</label>
+              <input
+                type="text"
+                id="firstName"
+                autoComplete="given-name"
+                className={styles.firstName}
+              />
+              <label htmlFor="lastName">Apelido</label>
+              <input
+                type="text"
+                id="lastName"
+                autoComplete="family-name"
+                className={styles.lastName}
+              />
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" className={styles.email} />
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                className={styles.password}
+              />
+              <button>Registar conta</button>
+            </div>
+          </form>
+        </div>
       </div>
     </>
   );
