bfed984207c95ef6420d0c21f1e8085c81024355
Bianca Vilaverde <biancamargarida2014@gmail.com>
2024-11-05 18:24:32 +0000

    logIn page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import SignUp from "./pages/Auth/SignUp";
+import LogIn from "./pages/Auth/LogIn";
 
 function App() {
   return (
     <>
+      <LogIn></LogIn>
       <SignUp></SignUp>
     </>
   );
---
+++ client/src/pages/Auth/LogIn.tsx
@@ -0,0 +1,69 @@
+import styles from "./LogIn.module.css";
+
+export default function LogIn() {
+  return (
+    <main className={styles.main}>
+      <section className={styles.left}>
+        <div className={styles.imgContainer}>
+          <img
+            src="WhatsApp Image 2024-11-04 at 03.39.22.jpeg"
+            alt="Purple and orange cloudy sky background"
+            loading="lazy"
+          />
+        </div>
+
+        <div className={styles.top}>
+          <div className={styles.logo}>AMU</div>
+          <div className={styles.back}>
+            <a href="/" className={styles.buttonSecondary}>
+              Back to website
+            </a>
+          </div>
+        </div>
+
+        <div className={styles.title}>
+          <h1>Explore. Sonhe. Descubra.</h1>
+          <h3>Conectando seus sonhos às melhores oportunidades</h3>
+        </div>
+      </section>
+
+      <section className={styles.right}>
+        <div className={styles.formContainer}>
+          <h1>Welcome Back!</h1>
+          <p>
+            Dont have an account? Create one! <a href="/signup">SignUp</a>
+          </p>
+
+          <form method="POST" autoComplete="off" noValidate>
+            <div className={styles.inputContainer}>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Email"
+                required
+                autoComplete="username"
+              />
+            </div>
+
+            <div className={styles.inputContainer}>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Password"
+                required
+                minLength={8}
+                autoComplete="new-password"
+              />
+            </div>
+
+            <button type="submit" className={styles.buttonPrimary}>
+              Login
+            </button>
+          </form>
+        </div>
+      </section>
+    </main>
+  );
+}
