024af04b54a86032d651daa87f3153bf5688856b
Bianca Vilaverde <biancamargarida2014@gmail.com>
2024-11-07 00:32:29 +0000

    navbar

--- client/src/components/layout/Navbar/Navbar.tsx
+++ client/src/components/layout/Navbar/Navbar.tsx
@@ -1,59 +1,34 @@
+import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
-
-// export default function Navbar() {
-//   return (
-//     <div className={styles.headercontainer}>
-//       <div className={styles.Navbar}>
-//         <div className={styles.leftnavbar}>
-//           <h1> Finder your dream job here! </h1>
-//           <div className={styles.logo}>AMU</div>
-//           <div className={styles.searchbar}>
-//             <input
-//               type="Job Search"
-//               id="Job Search"
-//               name="Job Search"
-//               placeholder="Job Search"
-//             />
-//             <input
-//               type="add zip code"
-//               id="add zip code"
-//               name="add zip code"
-//               placeholder="add zip code"
-//             />
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
+import { Bell, UserPen } from "lucide-react";
 
 export default function Navbar() {
   return (
-    <div className={styles.navbar}>
-      <input
-        type="Find jobs"
-        id="Find jobs"
-        name="Find jobs"
-        placeholder="Find Jobs"
-      />
-      <input
-        type="Find Talent"
-        id="Find Talent"
-        name="Find Talent"
-        placeholder="Find Talent"
-      />
-      <input
-        type="Upload Job"
-        id="Upload Job"
-        name="Upload Job"
-        placeholder="Upload Job"
-      />
-      <input
-        type="About Us"
-        id="About Us"
-        name="About Us"
-        placeholder="About Us"
-      />
-    </div>
+    <nav className={styles.navbar}>
+      <div className={styles.leftnavbar}>
+        <div className={styles.logo}>AMU</div>
+      </div>
+      <div className={styles.centernavbar}>
+        <Link className={styles.link} to={"/"}>
+          Find Jobs{" "}
+        </Link>
+        <Link className={styles.link} to={"/"}>
+          Upload Job{" "}
+        </Link>
+        <Link className={styles.link} to={"/"}>
+          About Us{" "}
+        </Link>
+      </div>
+
+      <div className={styles.rightnavbar}>
+        <Link className={styles.link} to={"/"}>
+          <Bell size={24} />
+        </Link>
+        <Link className={styles.link} to={"/"}>
+          <p className={styles.nome}>José Silva</p>
+          <UserPen />
+        </Link>
+      </div>
+    </nav>
   );
 }
