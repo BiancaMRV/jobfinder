f4049a30a58515f31b7d50fc304ac6e093fe81e7
Bianca Vilaverde <biancamargarida2014@gmail.com>
2024-11-26 18:18:25 +0000

    structure of job card

--- client/src/components/jobs/JobCard/JobCard.tsx
+++ client/src/components/jobs/JobCard/JobCard.tsx
@@ -1,30 +1,26 @@
 import styles from "./JobCard.module.css";
-import { useState } from "react";
 
 export default function JobCard() {
-  const [joboffers, setjoboffers] = useState([]);
-
-  const fetchdata = async () => {
-    try {
-      const response = await fetch("http://localhost:5000/jobs/filter");
-      const data = await response.json();
-      setjoboffers(data);
-    } catch (error) {
-      console.log("Error fetching data", error);
-    }
-  };
-
   return (
     <div className={styles.jobcontainer}>
-      <div className={styles.titlejobofferandlogo}>
-        <img
-          src="company-logo.png"
-          alt="Company Logo"
-          className="company-logo"
-        />
-        <h2 className={styles.joboffertitle}> job offer </h2>
+      <div className={styles.headercontainer}>
+        <div className={styles.titlejobofferandlogo}>
+          <img
+            src="company-logo.png"
+            alt="Company Logo"
+            className="company-logo"
+          />
+          <h2 className={styles.joboffertitle}> job offer </h2>
+        </div>
+        <div className={styles.tagcontainerexperiencelevel}>
+          <span className={styles.tag}>Entry Level</span>
+          <span className={styles.tag}>Full-Time</span>
+          <span className={styles.tag}>Remote</span>
+        </div>
+        <div className={styles.jobdescriptioncontainer}>
+          <p className={styles.jobdescription}>começo da descricao</p>
+        </div>
       </div>
-      <div className={styles.experiencelevellabel}></div>
     </div>
   );
 }
