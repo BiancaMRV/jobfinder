ab0084c90a46733a830d1d7938d81baa370df388
Bianca Vilaverde <biancamargarida2014@gmail.com>
2025-02-19 21:33:48 +0000

    added overview to profile

--- client/src/components/profile/Profile/Overview.tsx
+++ client/src/components/profile/Profile/Overview.tsx
@@ -6,6 +6,38 @@ export default function Overview() {
     <div className={styles.overviewcontainer}>
       <div className={styles.experience}>
         <Briefcase size={24} />
+        <span> Experience </span>
+        <Pencil size={24} />
+      </div>
+      <div className={styles.inputcontaineroverview}>
+        <div className={styles.inputtilte}>
+          <input
+            type="title"
+            id="title"
+            name="title"
+            placeholder="Title"
+            required
+          />
+        </div>
+        <div className={styles.inputdateandcompany}>
+          <input
+            type="date and name of company"
+            id="date and name of company"
+            name="date and name of company"
+            placeholder="Date and Name of Company"
+            required
+          />
+        </div>
+        <div className={styles.inputdescription}>
+          <input
+            type="description"
+            id="description"
+            name="description"
+            placeholder="Description"
+            maxLength={100}
+            required
+          />
+        </div>
       </div>
     </div>
   );
